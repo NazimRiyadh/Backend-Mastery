8c3f677dd9962baee66c720740fbea62c753c7b9
NazimRiyadh <neariyadh786@gmail.com>
2026-03-06 00:18:17 +0600

    adding txt in gitignore

--- Nodejs_Sheriyans/src/db/db.js
+++ Nodejs_Sheriyans/src/db/db.js
@@ -2,5 +2,5 @@ const mongoose= require("mongoose")
 
 
 async function connectDb(params) {
-    await mongoose.connect()
+    await mongoose.connect("mongodb+srv://backend_complete:password_backend@backendcluster.hiumws9.mongodb.net/")
 }
